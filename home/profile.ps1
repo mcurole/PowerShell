1fd49f05e4c2ee6777fd6cb63c152d56d1bde81b
Mark Curole <mark.curole@shawinc.com>
2017-10-02 16:36:40 -0400

    Remove PSDrives related to google drive

--- home/profile.ps1
+++ home/profile.ps1
@@ -1,10 +1,7 @@
 Import-Module Pscx -arg "$(Split-Path $profile -parent)\Pscx.UserPreferences.ps1"
 
 New-PSDrive -Name Docs -PSProvider FileSystem -Root ([environment]::GetFolderPath('MyDocuments')) | Out-Null
-New-PSDrive -Name Mod -PSProvider FileSystem -Root ([environment]::GetFolderPath('MyDocuments') + "\WindowsPowerShell\Modules") | Out-Null
-New-PSDrive -Name GDrive -PSProvider FileSystem -Root ([environment]::GetFolderPath('UserProfile') + "\Google Drive") | Out-Null
 New-PSDrive -Name Downloads -PSProvider FileSystem -Root ([environment]::GetFolderPath('UserProfile') + "\Downloads") | Out-Null
-New-PSDrive -Name ISOs -PSProvider FileSystem -Root ("C:\ISOs") | Out-Null
 
 $PSDefaultParameterValues = @{
     "Enter-PSSession:Authentication" = "Credssp"
