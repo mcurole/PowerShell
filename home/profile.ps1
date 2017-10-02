a1e3fd917b87142ce83fb882941e952728daf647
mcurole <30864938+mcurole@users.noreply.github.com>
2017-10-02 03:12:01 -0400

    Update Prompt

--- home/profile.ps1
+++ home/profile.ps1
@@ -14,9 +14,47 @@ $PSDefaultParameterValues = @{
 Import-Module posh-docker
 Import-Module posh-git
 
-New-Alias -Name cvis -Value Clear-VIServers
-New-Alias -Name sde -Value Switch-DockerEngine
-New-Alias -Name rds -Value Connect-RdServer
-New-Alias -Name rh -Value Resolve-DnsName
-New-Alias -Name zip -Value Microsoft.PowerShell.Archive\Compress-Archive
-New-Alias -Name unzip -Value Microsoft.PowerShell.Archive\Expand-Archive
+New-Alias -Name cvis -Value Clear-VIServers -Force 
+New-Alias -Name sde -Value Switch-DockerEngine -Force
+New-Alias -Name rds -Value Connect-RdServer -Force
+New-Alias -Name rh -Value Resolve-DnsName -Force
+New-Alias -Name zip -Value Microsoft.PowerShell.Archive\Compress-Archive -Force
+New-Alias -Name unzip -Value Microsoft.PowerShell.Archive\Expand-Archive -Force
+
+Function Test-Administrator {
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+Function prompt {
+    $realLASTEXITCODE = $LASTEXITCODE
+
+    Write-Host
+
+    if (Test-Administrator) {  # Use different username if elevated
+        Write-Host "(Elevated) " -NoNewline -ForegroundColor White
+    }
+
+    Write-Host "$ENV:USERNAME@" -NoNewline -ForegroundColor DarkYellow
+    Write-Host "$ENV:COMPUTERNAME" -NoNewline -ForegroundColor Magenta
+
+    if ($s -ne $null) {  # color for PSSessions
+        Write-Host " (`$s: " -NoNewline -ForegroundColor DarkGray
+        Write-Host "$($s.Name)" -NoNewline -ForegroundColor Yellow
+        Write-Host ") " -NoNewline -ForegroundColor DarkGray
+    }
+
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    Write-Host $($(Get-Location) -replace ($env:USERPROFILE).Replace('\','\\'), "~") -NoNewline -ForegroundColor Blue
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+    Write-Host (Get-Date -Format G) -NoNewline -ForegroundColor DarkMagenta
+    Write-Host " : " -NoNewline -ForegroundColor DarkGray
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+
+    Write-VcsStatus
+
+    Write-Host ""
+
+    return "> "
+}
